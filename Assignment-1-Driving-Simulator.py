543637668631108e42917044aca7a56944139707
samuel12327 <samuel.putra101@gmail.com>
2017-09-23 13:54:34 +0700

    New Upload for Driving Sim Assignment

--- Assignment-1-Driving-Simulator.py
+++ Assignment-1-Driving-Simulator.py
@@ -1,7 +1,7 @@
 print("Welcome to Driving Simulator")
-a = int(input("Acceleration (m/s2): "))
 t = int(input("Duration (s): "))
-s = int(input("Distance (m): "))
+a = int(input("Acceleration (m/s2): "))
+s = int(input("Target Distance (m): "))
 speedlimit = "Speed limit is 60 m/s"
 maxspeed = a * t #runs formula : v = u + at
 for dur in range(0, t + 1): #dur represents duration of trip
@@ -13,10 +13,10 @@ for dur in range(0, t + 1): #dur represents duration of trip
 if distance>=s: #compares set distance with actual distance travelled
     print("The person reached the destination.", "(Reached ", distance, "m)")
 else:
-    print("The person did not reach the destination.", "(Reached ", distance, "m)")
-print("Set distance was", s, "m .")
+    print("The person did not reach the destination.", "(Reached", distance, "m)")
+print("Target distance was", s, "m .")
 if maxspeed > 60: #compares ,maxspeed with speed limit, in this case it is 60 m/s
-    print("The person went over the speed limit.", "(Max speed was ", maxspeed, "m/s)")
+    print("The person went over the speed limit.", "(Max speed was", maxspeed, "m/s)")
 else:
     print("The person did not go over the speed limit.", "(Max speed was ", maxspeed, "m/s)")
 print(speedlimit, ".")
