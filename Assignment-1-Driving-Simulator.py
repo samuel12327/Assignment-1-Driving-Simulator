a4e1a9e6a4f1f1abf8ce05c1706584c8d3ca68d7
samuel12327 <samuel.putra101@gmail.com>
2017-09-25 09:38:13 +0700

    Updated Driving Sim Assignment

--- Assignment-1-Driving-Simulator.py
+++ Assignment-1-Driving-Simulator.py
@@ -1,8 +1,8 @@
 print("Welcome to Driving Simulator")
+t = int(input("Time Spent on the Road (s): "))
 a = int(input("Acceleration (m/s2): "))
-t = int(input("Duration (s): "))
-s = int(input("Distance (m): "))
-speedlimit = "Speed limit is 60 m/s"
+s = int(input("Target Distance (m): "))
+speedlimit = 60
 maxspeed = a * t #runs formula : v = u + at
 for dur in range(0, t + 1): #dur represents duration of trip
     answer = (0.5 * a * (dur * dur)) #runs formula : S = (0.5)at2
@@ -13,11 +13,11 @@ for dur in range(0, t + 1): #dur represents duration of trip
 if distance>=s: #compares set distance with actual distance travelled
     print("The person reached the destination.", "(Reached ", distance, "m)")
 else:
-    print("The person did not reach the destination.", "(Reached ", distance, "m)")
-print("Set distance was", s, "m .")
-if maxspeed > 60: #compares ,maxspeed with speed limit, in this case it is 60 m/s
-    print("The person went over the speed limit.", "(Max speed was ", maxspeed, "m/s)")
+    print("The person did not reach the destination.", "(Reached", distance, "m)")
+print("Target distance was", s, "m .")
+if maxspeed > speedlimit: #compares ,maxspeed with speed limit, in this case it is 60 m/s
+    print("The person went over the speed limit.", "(Max speed was", maxspeed, "m/s)")
 else:
     print("The person did not go over the speed limit.", "(Max speed was ", maxspeed, "m/s)")
-print(speedlimit, ".")
-print("Average Speed was", int(distance/t), "m/s .")
+print("The Speed Limit was", speedlimit, "m/s.") #states the speed limit
+print("Average Speed was", int(distance/t), "m/s.")
